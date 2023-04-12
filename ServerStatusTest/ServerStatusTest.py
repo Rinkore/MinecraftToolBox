f561efa62b09e20ac882d6fd5d5b6b811f5a81b2
Rinkore <92674395+Rinkore@users.noreply.github.com>
2023-04-12 20:22:41 +0800

    重构，取消json，改用gui

--- ServerStatusTest/ServerStatusTest.py
+++ ServerStatusTest/ServerStatusTest.py
@@ -3,6 +3,8 @@ import socket
 import codecs
 import time
 import json
+import tkinter as tk
+from tkinter import ttk
 
 
 class MinecraftServerStatusTest:
@@ -57,10 +59,79 @@ async def print_ports_info(hostname, ports):
     for port, port_results in ports_info.items():
         delay_sum = sum(result[2] for result in port_results)
         avg_delay = delay_sum / len(port_results)
-        print(f"{hostname}:{port}----{avg_delay:.2f}ms delay, Info: {port_results[0][3]}")
 
-with open('./Server/ServerList.json') as ServerListJson:
-    data = json.load(ServerListJson)
-    ServerList = data['hosts']
-    for servers in ServerList:
-        asyncio.run(print_ports_info(servers['ip'], servers['ports']))
+        # 将数据添加到表格中
+        tree.insert("", "end", values=(port, f"{avg_delay:.2f}ms"
+                                       , json.loads(json.dumps(port_results[0][3]["online_players"]))
+                                       , json.loads(json.dumps(port_results[0][3]["max_players"]))
+                                       , json.loads(json.dumps(port_results[0][3]["version"]))
+                                       , json.dumps(port_results[0][3]["motd"])))
+
+
+def start_scan():
+    hostname = ip_entry.get()
+    start_port = int(start_port_entry.get())
+    try:
+        stop_port = int(end_port_entry.get())
+    except ValueError:
+        stop_port = start_port
+    ports = range(start_port, stop_port + 1)
+    asyncio.run(print_ports_info(hostname, ports))
+
+
+root = tk.Tk()
+root.title("Minecraft Server Status Scanner")
+root.geometry("600x400")
+for i in range(4):
+    root.grid_rowconfigure(i, weight=1)
+root.grid_rowconfigure(4, weight=99)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=10)
+
+ip_label = tk.Label(root, text="IP:")
+ip_label.grid(row=0, column=0)
+
+ip_entry = tk.Entry(root)
+ip_entry.grid(row=0, column=1, sticky="ew")
+ip_entry.insert(0, "ipv4.rinkore.com")
+
+start_port_label = tk.Label(root, text="Start Port（必填）:")
+start_port_label.grid(row=1, column=0)
+
+start_port_entry = tk.Entry(root)
+start_port_entry.grid(row=1, column=1, sticky="ew")
+
+end_port_label = tk.Label(root, text="End Port（可空）:")
+end_port_label.grid(row=2, column=0)
+
+end_port_entry = tk.Entry(root)
+end_port_entry.grid(row=2, column=1, sticky="ew")
+
+scan_button = tk.Button(root, text="Start Scan", command=start_scan)
+scan_button.grid(row=3, column=0, columnspan=2, sticky="ew")
+
+# 创建Treeview
+tree = ttk.Treeview(root, columns=("port", "delay", "online_players", "max_players", "version", "motd"), show="headings")
+tree.grid(row=4, column=0, columnspan=2, sticky="nsew")
+
+# 设置列标题
+tree.heading("port", text="Port")
+tree.heading("delay", text="Delay")
+tree.heading("version", text="version")
+tree.heading("motd", text="motd")
+tree.heading("online_players", text="online_players")
+tree.heading("max_players", text="max_players")
+
+
+def on_resize(event):
+    tree.column("#0", width=0)  # 隐藏列 ID
+    tree.column("port", width=int(event.width * 0.1))
+    tree.column("delay", width=int(event.width * 0.12))
+    tree.column("online_players", width=int(event.width * 0.15))
+    tree.column("max_players", width=int(event.width * 0.14))
+    tree.column("version", width=int(event.width * 0.1))
+
+
+tree.bind("<Configure>", on_resize)
+
+root.mainloop()
